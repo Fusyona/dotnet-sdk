5aad939925c84a3f9c820ff5f77c4e0044c1968c
leandro-driguez <leandro__rodriguez@outlook.com>
2022-09-08 19:27:19 -0400

    feat(api): add all functions of the Wallet endpoint

--- Fusyona.Dotnet.Sdk/Apis/Wallet/Wallet.cs
+++ Fusyona.Dotnet.Sdk/Apis/Wallet/Wallet.cs
@@ -1,4 +1,6 @@
 
+using Fusyona.Dotnet.Sdk;
+
 namespace Fusyona.Dotnet.Sdk.Apis.Wallet;
 
 public static class Wallet
@@ -11,6 +13,52 @@ public static class Wallet
     {
         var request = Utils.ConstructRequest(HttpMethod.Get, bearerToken, 
             subcriptionKey, baseUrl + $"addresses/{address_id}/balance");
+
+        return await client.SendAsync(request);
+    }
+
+    public static async Task<HttpResponseMessage> GetFullBalanceAsync(
+        string bearerToken, string subcriptionKey, string address_id) 
+    {
+        var request = Utils.ConstructRequest(HttpMethod.Get, bearerToken, 
+            subcriptionKey, baseUrl + $"addresses/{address_id}/fullbalance");
+
+        return await client.SendAsync(request);
+    }
+
+    public static async Task<HttpResponseMessage> GetClientAddressesAndBalanceAsync(
+        string bearerToken, string subcriptionKey) 
+    {
+        var request = Utils.ConstructRequest(HttpMethod.Get, bearerToken, 
+            subcriptionKey, baseUrl + $"fulladdresses");
+
+        return await client.SendAsync(request);
+    }
+
+    public static async Task<HttpResponseMessage> GetCryptocurrenciesAsync(
+        string bearerToken, string subcriptionKey) 
+    {
+        var request = Utils.ConstructRequest(HttpMethod.Get, bearerToken, 
+            subcriptionKey, baseUrl + $"cryptocurrencies");
+
+        return await client.SendAsync(request);
+    }
+
+    public static async Task<HttpResponseMessage> GetCryptocurrencySubUnistAsync(
+        string bearerToken, string subcriptionKey, string cryptocurrency_id) 
+    {
+        var request = Utils.ConstructRequest(HttpMethod.Get, bearerToken, 
+            subcriptionKey, baseUrl + $"cryptocurrencies/{cryptocurrency_id}/subunits");
+
+        return await client.SendAsync(request);
+    }
+
+    public static async Task<HttpResponseMessage> GetWithdrawalAsync(
+        string bearerToken, string subcriptionKey, string address_id, 
+        string to_public_key, float amount) 
+    {
+        var request = Utils.ConstructRequest(HttpMethod.Get, bearerToken, 
+            subcriptionKey, baseUrl + $"addresses/{address_id}/transfer/{to_public_key}/{amount}");
 
         return await client.SendAsync(request);
     }
